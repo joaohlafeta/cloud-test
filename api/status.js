d52ff5932460957f7ab5c6faf51373bc55fc1d8c
joaohlafeta <105346791+joaohlafeta@users.noreply.github.com>
2025-09-01 12:00:04 -0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
+// Export default é obrigatório para o Vercel mapear a rota
+export default function handler(req, res) {
   res.status(200).json({ status: "hello world" });
-};
+}
